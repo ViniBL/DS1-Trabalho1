9f470800d32160ac43756ef162df8c245d051764
MatosAugusto <augustomatos@estudante.ufscar.br>
2022-08-05 15:38:48 -0300

    add DestinoDAO

--- src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
+++ src/main/java/br/ufscar/dc/dsw/dao/UsuarioDAO.java
@@ -91,6 +91,7 @@ public class UsuarioDAO extends GenericDAO {
             statement.setString(2, usuario.getLogin());
             statement.setString(3, usuario.getSenha());
             statement.setString(4, usuario.getPapel());
+            statement.setString(5, usuario.getId());
             statement.executeUpdate();
 
             statement.close();
